2a82fbf228c08867da117e656615c8e17f5c7426
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-01-27 12:55:07 +0100

    del

--- 0x0C-Javascript_advanced/0-welcome.js
+++
@@ -1,7 +0,0 @@
-function welcome(firstName, lastName) {
-    let fullName = firstName + " " + lastName;
-    function displayFullName() {
-        alert("welcome " + fullName + "!");
-    }
-    displayFullName();
-  }
--- 0x0C-Javascript_advanced/5-mode.js
+++
@@ -1,7 +0,0 @@
-function changeMode(size, weight, transform, background, color) {
-    document.documentElement.style["font-size"] = size;
-    document.documentElement.style["font-weight"] = weight;
-    document.documentElement.style["text-transform"] = transform;
-    document.documentElement.style["background-color"] = background;
-    document.documentElement.style["color"] = color;
-}
